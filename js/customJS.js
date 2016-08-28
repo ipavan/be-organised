d6e459c7a5a93e999cc79619c0609e32f5244c57
Pavan Sadarangani <ipavan@gmail.com>
2016-08-28 15:15:33 +0100

    Adds edit functionality

--- js/customJS.js
+++ js/customJS.js
@@ -2,18 +2,31 @@ var counter = 0
 
 function addFunction() {
 	if (document.getElementById("description").value == "") {
+		//Nothing to be added if the to do list has no item to do
 		return;
 	}
+
     var tableVal = document.getElementById("tableBody");
     var newRow = tableVal.insertRow(-1);
+
+    //give each row a unique id
     newRow.id = counter;
     counter = counter + 1;
+
+    //insert date, description and delete cell buttons
     var date = newRow.insertCell(0);
     var description = newRow.insertCell(1);
     var delEd = newRow.insertCell(2);
-    date.innerHTML = document.getElementById("dateField").value;
-    description.innerHTML = document.getElementById("description").value;
+
+    //get the current date value
+    var someVal = document.getElementById("dateField").value;
+
+    //add content to each of the three cells in the row
+    date.innerHTML = '<input id = "dateField" type="date" value="' + someVal + '" class="form-control">';
+    description.innerHTML = '<div contentEditable>' + document.getElementById("description").value;
     delEd.innerHTML = '<button type="button" id="' + counter + '" class="btn btn-danger" onclick = "deleteFunction(this)">x</button>';
+
+    //reset the description input
     document.getElementById("description").value = "";
 }
 
@@ -22,13 +35,4 @@ function deleteFunction(x) {
     document.getElementById("mainTable").deleteRow(row);
 }
 
-/*$(document).ready (function(){
-    $("#warning-alert").hide();
-    $("#addButton").click(function showAlert() {
-        $("#warning-alert").alert();
-        $("#warning-alert").fadeTo(2000, 500).slideUp(500, function(){
-            $("#warning-alert").slideUp(500);
-        });
-    });
-});*/
 
